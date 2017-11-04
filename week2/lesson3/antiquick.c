9d42007ecf4b4b710b2ec69ed71ce08a176fc08e
andrejlevkovitch <andrejlevkivitch@gmail.com>
2017-11-04 21:25:22 +0300

    попытка решить 3 задачу - в прогрессе

--- week2/lesson3/antiquick.c
+++ week2/lesson3/antiquick.c
@@ -3,18 +3,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void generation (int masElements [], int first, int last, int *kurrent)
+void generation (int masElements [], int anti [], int first, int last, int *kurrent)
 {
-    int midle = first + (last - first) / 2;
+    int left = first;
+    int right = last;
+    int midle = (left + right) / 2;
+    int temp = 0;
 
-    masElements [midle] = *kurrent;
-    --*kurrent;
+    if (masElements [midle] == 1) {
+        midle = masElements [midle] = *kurrent;
+        --*kurrent;
+    }
 
-    if (midle + 1 != last)
-    generation (masElements, midle + 1, last, kurrent);
+    while (left <= right) {
+        while (masElements [left] < midle) {
+            ++left;
+        }
 
-    if (midle != first)
-    generation (masElements, first, midle, kurrent);
+        while (masElements [right] > midle) {
+            --right;
+        }
+
+        if (left <= right) {
+            temp = masElements [left];
+            masElements [left] = masElements [right];
+            masElements [right] = temp;
+
+            temp = anti [left];
+            anti [left] = anti [right];
+            anti [right] = temp;
+
+            ++left;
+            --right;
+        }
+    }
+
+    if (first < right) {
+        generation (masElements, anti, first, right, kurrent);
+    }
+
+    if (last > left) {
+        generation (masElements, anti, left, last, kurrent);
+    }
 }
 
 int main (void)
@@ -38,21 +68,39 @@ int main (void)
         exit (EXIT_FAILURE);
     }
 
-    int *masElements = malloc (size * sizeof (int));
+    int *masElements = malloc (size * sizeof (*masElements));
+    int *rezult = malloc (size * sizeof (*rezult));
 
-    generation (masElements, 0, size - 1, kurrent);
+    for (unsigned int i = 0; i < size; ++i) {
+        masElements [i] = 1;
+    }
+
+    int *anti = malloc (size * sizeof (*anti));
+
+    for (unsigned int i = 0; i < size; ++i) {
+        anti [i] = size - 1 - i;
+    }
+
+    generation (masElements, anti, 0, size - 1, kurrent);
+
+    for (unsigned int i = 0; i < size; ++i) {
+        rezult [i] = masElements [anti [i]];
+    }
 
     if ((output = fopen ("output.txt", "w")) == NULL) {
         printf ("ERROR of open file output.txt\n");
         exit (EXIT_FAILURE);
     }
 
-    for (int i = size - 1; i >= 0; --i) {
-        if (masElements [i] == 0) 
-            masElements [i] = 1;
-        fprintf (output, "%i ", masElements [i]);
+//    if (size == 4) {
+  //      fprintf (output, "3 4 1 2");
+ //   }
+//    else {
+    for (unsigned int i = 0; i < size; ++i) {
+        fprintf (output, "%i ", rezult [i]);
     }
-
+//    }
+    
     if (fclose (output) != 0) {
         printf ("ERROR of exit from file output.txt\n");
         exit (EXIT_FAILURE);
